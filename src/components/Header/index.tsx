f675b5ca7560ba612700cce7490ca52e57a14cbf
Monique Melo <jmoniquemelo@gmail.com>
2024-08-18 01:13:27 -0300

    Alterações no componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
-import { Link } from "react-router-dom";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { useEffect, useState, useRef } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { IoIosMenu } from "react-icons/io";
+import { IoCloseOutline } from "react-icons/io5";
+import { SlArrowRight } from "react-icons/sl";
 import Logo from "../Logo";
 import "./header.css";
-import { useEffect, useState } from "react";
 
 function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
+  const headerRef = useRef<HTMLDivElement | null>(null);
+  const location = useLocation();
 
+  
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
@@ -27,14 +33,34 @@ function Header() {
     };
 
     window.addEventListener("scroll", scrollHeader);
-
+    
     return () => {
       window.removeEventListener("scroll", scrollHeader);
     };
   }, []);
 
+
+  useEffect(() => {
+    const adjustBodyPadding = () => {
+      if (headerRef.current) {
+        document.body.style.paddingTop = `${headerRef.current.offsetHeight}px`;
+      }
+    };
+
+    adjustBodyPadding();
+    window.addEventListener("resize", adjustBodyPadding);
+
+    return () => {
+      window.removeEventListener("resize", adjustBodyPadding);
+    };
+  }, []);
+
+
   return (
-    <header className={`header ${isScrolled ? "shadow-header" : ""}`}>
+    <header
+      ref={headerRef}
+      className={`header ${isScrolled ? "shadow-header" : ""}`}
+    >
       <div className="header-content">
         <Link to="/" onClick={scrollUp}>
           <Logo />
@@ -42,23 +68,57 @@ function Header() {
 
         <nav className={`nav ${isMenuOpen ? "open" : ""}`}>
           <ul className="nav-list">
-            <Link to="/informe-se" className="header-btn" onClick={scrollUp}>
-              <li className="nav-list-item">Informe-se</li>
+            <Link
+              to="/informe-se"
+              className={`header-btn ${
+                location.pathname === "/informe-se" ? "active" : ""
+              }`}
+              onClick={scrollUp}
+            >
+              <li className="nav-list-item">
+                Informe-se
+                <SlArrowRight className="menu-arrow" />
+              </li>
             </Link>
 
-            <Link to="/calculadora" className="header-btn" onClick={scrollUp}>
-              <li className="nav-list-item">Calculadora de saúde</li>
+            <Link
+              to="/calculadora"
+              className={`header-btn ${
+                location.pathname === "/calculadora" ? "active" : ""
+              }`}
+              onClick={scrollUp}
+            >
+              <li className="nav-list-item">
+                Calculadora de saúde
+                <SlArrowRight className="menu-arrow" />
+              </li>
             </Link>
 
-            <li className="nav-list-item">Encontrar ajuda</li>
+            <li className="nav-list-item">
+              Encontrar ajuda
+              <SlArrowRight className="menu-arrow" />
+            </li>
 
-            <Link to="/sobre-nos" className="header-btn" onClick={scrollUp}>
-              <li className="nav-list-item">Sobre nós</li>
+            <Link
+              to="/sobre-nos"
+              className={`header-btn ${
+                location.pathname === "/sobre-nos" ? "active" : ""
+              }`}
+              onClick={scrollUp}
+            >
+              <li className="nav-list-item">
+                Sobre nós
+                <SlArrowRight className="menu-arrow" />
+              </li>
             </Link>
           </ul>
         </nav>
         <div className="menu-icon" onClick={toggleMenu}>
-          {isMenuOpen ? <FaTimes size={40} /> : <FaBars size={40} />}
+          {isMenuOpen ? (
+            <IoCloseOutline size={40} fontWeight={400} />
+          ) : (
+            <IoIosMenu size={40} />
+          )}
         </div>
       </div>
     </header>

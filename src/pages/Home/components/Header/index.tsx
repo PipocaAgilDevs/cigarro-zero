1773772d799608ab7be76cfaa7106beb6f4a4a76
Monique Melo <jmoniquemelo@gmail.com>
2024-08-24 15:23:23 -0300

    Refatoração do Header da Home

--- src/pages/Home/components/Header/index.tsx
+++ src/pages/Home/components/Header/index.tsx
@@ -1,28 +1,28 @@
 import heroImage from "@assets/hero-image.svg";
 
-import "./Hero.modules.css";
+import "./styles.css";
 
 export default function Header() {
   return (
-    <section className="banner">
-      <div className="banner-content">
-        <div className="banner-content-text">
-          <p className="banner-title">COMECE UMA VIDA</p>
-          <p className="banner-second-title">
-            SAÚDAVEL E <span className="colored-text">SEM CIGARRO</span>
+    <section className="banner__header">
+      <div className="banner__content">
+        <div className="banner__content__text">
+          <p className="banner__title">COMECE UMA VIDA</p>
+          <p className="banner__second__title">
+            SAÚDAVEL E <span className="banner__second_title--color">SEM CIGARRO</span>
           </p>
-          <div className="sub-text-section">
-            <p className="banner-subtitle">
+          <div className="banner__content__subtext">
+            <p className="banner__subtitle">
               Nossa missão é fornecer informações valiosas e recursos de apoio
               para todos que desejam parar de fumar.{" "}
             </p>
-            <p className="banner-subtitle">
+            <p className="banner__subtitle">
               Navegue pelo nosso site e encontre tudo que você precisa para
               começar sua jornada livre do cigarro!
             </p>
           </div>
         </div>
-        <img src={heroImage} alt="Foto depoimentos" />
+        <img className="banner__image" src={heroImage} alt="três pessoas sorrindo e abraçadas dando depoimentos sobre estar sem fumar, como 'um dia de cada vez', 'juntos somos mais fortes' e 'menos um cigarro hoje'" />
       </div>
     </section>
   );

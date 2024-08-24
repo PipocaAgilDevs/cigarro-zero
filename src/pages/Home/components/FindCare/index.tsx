bbdb0ca20102df8e5dd9f7c4a39d08e6f2ec1afe
Monique Melo <jmoniquemelo@gmail.com>
2024-08-24 15:22:59 -0300

    Refatoração da FindCare

--- src/pages/Home/components/FindCare/index.tsx
+++ src/pages/Home/components/FindCare/index.tsx
@@ -1,19 +1,28 @@
+import { useNavigate } from "react-router-dom";
 import ButtonCTA from "../../../../components/ButtonCTA";
 import "./styles.css";
 
-export default function FindHelp() {
+export default function FindCare() {
+
+  const navigate = useNavigate();
+
   return (
-    <section className="find-help-section">
-      <section className="container-cta">
-        <p className="title-cta">
+    <section className="find-care">
+      <section className="find-care__container">
+        <h2 className="find-care__title">
           A luta contra o tabagismo <br></br>não precisa ser solitária
-        </p>
-        <p className="sub-text-cta">
-          Buscar ajuda profissional aumenta significativamente o sucesso de
-          conseguir parar de fumar.<br></br> O Sistema Único de Saúde (SUS)
-          oferece tratamentos integrais e gratuitos!
-        </p>
-        <ButtonCTA>Encontre atendimento perto de você</ButtonCTA>
+        </h2>
+        <div className="find-care__container__text">
+          <p className="find-care__subtitle">
+            Buscar ajuda profissional aumenta significativamente o sucesso de
+            conseguir parar de fumar.
+          </p>
+          <p className="find-care__subtitle">
+            O Sistema Único de Saúde (SUS)
+            oferece tratamentos integrais e gratuitos!
+          </p>
+        </div>
+        <ButtonCTA onClick={() => navigate("/encontrar-ajuda")} >Encontre atendimento perto de você</ButtonCTA>
       </section>
     </section>
   );
